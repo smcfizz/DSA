3f4b15ee7c6217cb0e00ade1bd4fcd98226d65c7
Benjamin Lane <ben@benlane.dev>
2025-02-20 16:02:44 -0800

    Implement DisjointSet data structure

---
+++ ds/disjoint_set.py
@@ -0,0 +1,56 @@
+from ds.hash_map import HashMap
+
+
+class DisjointSet[T]:
+    """
+    Disjoint set data structure utilizing union by rank and path compression optimizations
+
+    With the above-mentioned optimizations, find() and union() have a runtime of O(a(n))
+    where a(n) is the inverse Ackermann function. For practical purposes, we can treat this O(1)
+    """
+    def __init__(self):
+        self._parent = HashMap()
+        self._rank = HashMap()
+
+    def make_set(self, nums: list[T]):
+        for n in nums:
+            self._parent[n] = n
+            self._rank[n] = 0
+
+    def find(self, n: T):
+        if n not in self._parent:
+            return None
+        if self._parent[n] != n:
+            # path compression, flatten the tree by raising all nodes to the root
+            self._parent[n] = self.find(self._parent[n])
+        return self._parent[n]
+
+    def union(self, n1: T, n2: T):
+        p1 = self.find(n1)
+        p2 = self.find(n2)
+        if p1 == p2:
+            return
+        # union by rank, merge trees with lower rank into the tree with higher rank
+        # to prevent tree growth and improve performance of find() by keeping trees small
+        if self._rank[p1] > self._rank[p2]:
+            self._parent[p2] = p1
+        elif self._rank[p1] < self._rank[p2]:
+            self._parent[p1] = p2
+        else:
+            self._parent[p2] = p1
+            self._rank[p1] += 1
+
+
+if __name__ == '__main__':
+    universe = [1, 2, 3, 4, 5]
+    ds = DisjointSet()
+    ds.make_set(universe)
+    ds.union(1, 2)
+    ds.union(2, 3)
+    ds.union(3, 4)
+    ds.union(4, 5)
+    print(ds.find(1))
+    print(ds.find(2))
+    print(ds.find(3))
+    print(ds.find(4))
+    print(ds.find(5))
